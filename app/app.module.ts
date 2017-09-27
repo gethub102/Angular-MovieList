519d138ff385c27fab89a98d9f5d53358d65363d
Steven <sunetwli102@gmail.com>
2017-09-27 12:23:22 -0700

    Init Angular Project  -install npm packages - create app.module and first component

---
+++ app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core"; 
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./component.app";
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [],
+  bootstrap: []
+})
+export class AppModule {}
